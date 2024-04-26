3b75cbfd2c721900626a13a4d5efe70d2242b926
Robin <robin.django@gmail.com>
2024-04-26 10:22:04 +0200

    Clientside JS start

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,5 @@
+import { query } from "express";
+
 const numberWithPeriods = (value) => {
   return value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 };
@@ -7,5 +9,29 @@ prices = document.querySelectorAll(".price");
 prices.forEach((price) => {
   const priceValue = price.innerHTML.toString().replace("€", "");
   price.innerHTML = `€${numberWithPeriods(priceValue)}`;
+});
 
+// Clientside POST
+
+document.addEventListener("DOMContentLoaded", () => {
+  const forms = document.querySelectorAll(".rating-form");
+
+  forms.forEach((form) => {
+    form.addEventListener("submit", (event) => {
+      const id = form.dataset.id;
+      const rating = form.querySelector("input[name=rating]:checked").value;
+
+console.log
+
+      fetch(`/rate/${id}/${rating}`, {
+        method: "POST",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+
+      event.preventDefault();
+    });
+  });
 });
